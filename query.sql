93bbbbc5e5ff3227672b88a0e0d6cd7ddeb3ba8c
charlesanderson25 <charles_spfc@hotmail.com>
2025-01-23 20:21:06 -0300

    Update file

--- query.sql
+++ query.sql
@@ -96,3 +96,97 @@ CREATE TABLE clientes (
        endereco VARCHAR(255),
        PRIMARY KEY (id)
 )
+
+-- UNION
+
+SELECT RUA, 
+		 BAIRRO, 
+		 CIDADE, 
+		 ESTADO, 
+		 CEP 
+FROM alura.tb_colaboradoes
+
+UNION ALL
+
+SELECT RUA,
+
+		 BAIRRO,
+		 CIDADE,
+		 ESTADO,
+		 CEP 
+FROM ALURA.tb_fornecedores;
+
+SELECT * FROM
+ALURA.tb_colaboradoes;
+
+SELECT * FROM 
+ALURA.tb_pedidos;
+
+SELECT * FROM
+ALURA.tb_itenspedidos;
+
+SELECT * FROM 
+ALURA.tb_CLIENTES;
+
+SELECT * FROM
+ALURA.tb_fornecedores;
+
+-- Insere dados a partir de um arquivp .csv
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/pedidos.csv'
+INTO TABLE ALURA.tb_pedidos
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(ID, IDCLIENTE, DATAHORAPEDIDO, STATUS);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/itens_de_pedido.csv'
+INTO TABLE ALURA.tb_itenspedidos
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(IDPEDIDO, IDPRODUTO, QUANTIDADE, PRECOUNITARIO);
+
+CREATE TEMPORARY TABLE alura.tb_pedidos_temp (
+    ID INT,
+    IDCLIENTE INT,
+    DATAHORAPEDIDO DATETIME,
+    STATUS VARCHAR(50)
+);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/pedidos.csv'
+INTO TABLE alura.tb_pedidos_temp
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(ID, IDCLIENTE, DATAHORAPEDIDO, STATUS);
+
+-- Subconsultas
+
+SELECT DISTINCT idcliente
+FROM ALURA.tb_pedidos_TEMP
+WHERE IDCLIENTE NOT IN (SELECT ID FROM ALURA.tb_clientes);
+
+SELECT * FROM
+alura.tb_pedidos_temp;
+
+SELECT * FROM 
+ALURA.tb_pedidos
+WHERE ( SELECT ID FROM ALURA.tb_clientes WHERE ID = 10 );
+
+SELECT * 
+FROM ALURA.tb_clientes;
+
+SELECT * 
+FROM ALURA.tb_PEDIDOS;
+
+SELECT * 
+FROM ALURA.tb_clientes
+	WHERE ID = ( 
+		SELECT IDCLIENTE 
+			FROM ALURA.tb_pedidos 
+			WHERE ID = 1 );
+			
+SELECT * 
+FROM ALURA.tb_clientes
+WHERE ID IN (SELECT IDCLIENTE FROM ALURA.tb_pedidos WHERE DATE_FORMAT(tb_pedidos.DATAHORAPEDIDO, '%d') = '02');
